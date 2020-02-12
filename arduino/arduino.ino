f7669b9dfc3a263f1919797d96dba95b0731b53d
Timothy Yong <tyong_23@hotmail.com>
2020-02-11 19:50:31 -0800

    first test

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,14 +3,38 @@
      by Dejan, https://howtomechatronics.com
 */
 #include <Servo.h>
-Servo ESC;     // create servo object to control the ESC
-int potValue;  // value from the analog pin
+Servo drive;     // create servo object to control the ESC
+static int drive_vel;
+Servo wheel;
+static int wheel_dir;
 void setup() {
+  Serial.begin(57600);
   // Attach the ESC on pin 9
-  ESC.attach(9,10000,20000); // (pin, min pulse width, max pulse width in microseconds) 
+  drive.attach(9,10000, 20000); // (pin, min pulse width, max pulse width in microseconds)
+  drive.write(45);
+  wheel.attach(7,10000, 20000);
+  wheel.write(90);
 }
+
 void loop() {
-  //potValue = 512;
-  //potValue = map(potValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
-  ESC.write(135);    // Send the signal to the ESC
+  if (Serial.available()) {
+    char c = Serial.read();
+    int vel = (int)c & 0x7F;
+    int id = ((int)c & 0x8) >> 7;
+    if (vel < 0) {
+      vel = 0;
+    }
+    if (vel > 90) {
+      vel = 90;
+    }
+    vel *= 2;
+    if (id == 0) {
+      drive_vel = vel;
+      drive.write(drive_vel);
+    }
+    if (id == 1) {
+      wheel_dir = vel;
+      wheel.write(wheel_dir);
+    }
+  }
 }
